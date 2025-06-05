64035359e552035fc60d4eac1b3395ba2b474f5c
Hoon-Gyu Chung <107260735+hg-chung@users.noreply.github.com>
2025-06-05 12:41:27 +0900

    Update train.sh
    Signed-off-by: Hoon-Gyu Chung <107260735+hg-chung@users.noreply.github.com>

--- train.sh
+++ train.sh
@@ -20,6 +20,6 @@ python train.py -s data/real1  \
                 --gamma
                 
 python train.py -s data/real2  \
-                --lambda_weight_sparse 0.005 \
+                --lambda_weight_img_sparse 0.005 \
                 --basis_merge_threshold 0.015 \
                 --gamma
